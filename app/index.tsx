ef6d037c2b12feb80a159e7b7bb623032ce7b816
Omkar Potphode <omkar03potphode@gmail.com>
2024-04-28 01:41:00 +0530

    ✨ Added signup screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,10 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useAssets } from 'expo-asset'
 import { ResizeMode, Video } from 'expo-av';
+import { Link } from 'expo-router';
+import { defaultStyles } from '@/constants/Styles';
+import Colors from '@/constants/Colors';
 
 const Page = () => {
 
@@ -23,6 +26,26 @@ const Page = () => {
             <View style={{ marginTop: 80, padding: 20 }}>
                 <Text style={styles.header}>Redefining your money mindset.</Text>
             </View>
+
+            <View style={styles.buttons}>
+                <Link href={'/login'}
+                style={[defaultStyles.pillButton, {flex: 1, backgroundColor: Colors.dark}]}
+                asChild
+                >
+                    <TouchableOpacity>
+                        <Text style={{ color: 'white', fontSize: 22, fontWeight: '500'}}>Log in</Text>
+                    </TouchableOpacity>
+                </Link>
+
+                <Link href={'/signup'}
+                style={[defaultStyles.pillButton, {flex: 1, backgroundColor: "#fff"}]}
+                asChild
+                >
+                    <TouchableOpacity>
+                        <Text style={{ fontSize: 22, fontWeight: '500'}}>Sign up</Text>
+                    </TouchableOpacity>
+                </Link>
+            </View>
         </View>
     )
 }
@@ -43,7 +66,14 @@ const styles = StyleSheet.create({
         textTransform: 'uppercase',
         color: 'white',
     },
+    buttons:{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 20,
+        marginBottom: 60,
+        paddingHorizontal: 20,
+    },
 
-})
+});
 
 export default Page;
--- app/signup.tsx
+++ app/signup.tsx
@@ -1,12 +1,85 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TextInput, KeyboardAvoidingView, Keyboard, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { defaultStyles } from '@/constants/Styles';
+import Colors from '@/constants/Colors';
+import { Link, useRouter } from 'expo-router';
 
-const signup = () => {
+const Page = () => {
+
+  const [countryCode, setCountryCode] = useState('+91');
+  const [phoneNum, setPhoneNum] = useState('');
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>signup</Text>
-    </View>
+    <KeyboardAvoidingView
+    style={{flex: 1}}
+    behavior='padding'
+    >
+      <View style={defaultStyles.container}>
+        <Text style={defaultStyles.header}>Let's get started</Text>
+        <Text style={defaultStyles.descriptionText}>
+          Enter your phone no. We will send you a confirmation code there
+        </Text>
+
+        <View style={styles.inputContainer}>
+          <TextInput
+          style={styles.input}
+          placeholderTextColor={Colors.gray}
+          value={countryCode}
+          />
+
+          <TextInput
+          style={[styles.input, {flex: 1}]}
+          placeholder='Mobile Number'
+          placeholderTextColor={Colors.gray}
+          keyboardType='numeric'
+          value={phoneNum}
+          onChangeText={setPhoneNum}
+          />
+        </View>
+
+        <Link href={'/login'} replace asChild>
+          <TouchableOpacity>
+            <Text style={defaultStyles.textLink}>
+              Already have an account? 
+            </Text>
+          </TouchableOpacity>
+        </Link>
+
+        <View style={{flex:1 }}/>
+
+        <TouchableOpacity style={[ 
+          defaultStyles.pillButton,
+          phoneNum !== "" ? styles.enabled : styles.disabled,
+          {marginBottom: 10},
+        ]}>
+          <Text>Sign up</Text>
+        </TouchableOpacity>
+
+      </View>
+    </KeyboardAvoidingView>
   )
 }
 
-export default signup;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginVertical: 40,
+    flexDirection: 'row',
+  },
+  input: {
+    backgroundColor: Colors.lightGray,
+    padding: 20,
+    borderRadius: 16,
+    fontSize: 20,
+    marginRight: 10,
+  },
+  enabled:{
+    backgroundColor: Colors.primary,
+  },
+  disabled: {
+    backgroundColor: Colors.primaryMuted,
+  }
+})
+
+export default Page;
