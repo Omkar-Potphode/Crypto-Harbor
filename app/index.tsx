5be784b5e38fb252d1db855060404f7e6f08da28
Omkar Potphode <omkar03potphode@gmail.com>
2024-04-28 00:45:14 +0530

    ✨ Implemented the intro screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,49 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { useAssets } from 'expo-asset'
+import { ResizeMode, Video } from 'expo-av';
 
 const Page = () => {
-  return (
-    <View>
-      <Text>Page</Text>
-    </View>
-  )
+
+    const [assets] = useAssets([require('@/assets/videos/intro.mp4')]);
+
+    return (
+        <View style={styles.container}>
+            { assets && (
+                <Video 
+                source={{ uri: assets[0].uri}} 
+                style={styles.video}
+                resizeMode={ResizeMode.COVER}
+                isLooping
+                isMuted
+                shouldPlay
+                />
+            )}
+
+            <View style={{ marginTop: 80, padding: 20 }}>
+                <Text style={styles.header}>Redefining your money mindset.</Text>
+            </View>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between',
+    },
+    video:{
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+    },
+    header:{
+        fontSize: 36,
+        fontWeight: '900',
+        textTransform: 'uppercase',
+        color: 'white',
+    },
+
+})
 
 export default Page;
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,8 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
-
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+  primary: '#4D3FE3',
+  primaryMuted: '#BFAEFF',
+  background: '#F7F7F7',
+  dark: '#333333',
+  gray: '#5C7080',
+  lightGray: '#C0C0C0',
 };
